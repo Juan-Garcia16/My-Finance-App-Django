e2367f3a2cd3727f5f57680330eaa3dda3a97ea4
Juan Garcia <juan.garcia16@utp.edu.co>
2025-11-18 14:03:19 -0500

    postgrest conectado

--- myfinanceapp/asgi.py
+++ myfinanceapp/asgi.py
@@ -1,16 +1,16 @@
 """
-ASGI config for myfinanceapp project.
+ASGI config for MyFinanceApp project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
 
 import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myfinanceapp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MyFinanceApp.settings')
 
 application = get_asgi_application()
--- myfinanceapp/wsgi.py
+++ myfinanceapp/wsgi.py
@@ -1,16 +1,16 @@
 """
-WSGI config for myfinanceapp project.
+WSGI config for MyFinanceApp project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myfinanceapp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MyFinanceApp.settings')
 
 application = get_wsgi_application()
