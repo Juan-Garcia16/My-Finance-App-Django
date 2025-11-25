6af854742aa9b1e2bfd57fb224ae2f3edd7a737b
Juan Garcia <juan.garcia16@utp.edu.co>
2025-11-25 14:21:29 -0500

    sercicios de goals

---
+++ goals/services/goal_manager.py
@@ -0,0 +1,18 @@
+from goals.models import MetaAhorro
+
+class GoalManager:
+    def __init__(self, usuario):
+        self.usuario = usuario
+
+    def crear_meta(self, nombre, monto_objetivo, fecha_limite):
+        return MetaAhorro.objects.create(
+            usuario=self.usuario,
+            nombre=nombre,
+            monto_objetivo=monto_objetivo,
+            fecha_limite=fecha_limite
+        )
+
+    def añadir_progreso(self, meta_id, monto):
+        meta = MetaAhorro.objects.get(id=meta_id, usuario=self.usuario)
+        meta.actualizar_progreso(monto)
+        return meta
