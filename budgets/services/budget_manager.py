bff3781fcd697b1f371655b3c9d784afaab62ba8
Juan Garcia <juan.garcia16@utp.edu.co>
2025-11-25 14:19:22 -0500

    serviciones de presupuestos (budget)

---
+++ budgets/services/budget_manager.py
@@ -0,0 +1,25 @@
+from budgets.models import Presupuesto
+from categories.models import Category
+
+class BudgetManager:
+    def __init__(self, usuario):
+        self.usuario = usuario
+
+    def crear_presupuesto(self, categoria_id, limite, mes):
+        categoria = Category.objects.get(id=categoria_id, usuario=self.usuario)
+        return Presupuesto.objects.create(
+            usuario=self.usuario,
+            categoria=categoria,
+            limite=limite,
+            mes=mes
+        )
+
+    def estado_presupuesto(self, categoria_id):
+        try:
+            presupuesto = Presupuesto.objects.get(
+                usuario=self.usuario,
+                categoria_id=categoria_id
+            )
+            return presupuesto.verificar_limite()
+        except Presupuesto.DoesNotExist:
+            return None
