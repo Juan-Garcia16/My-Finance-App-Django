6923c3d64824ed05895fb0af383b7c76f7cdd15e
Juan Garcia <juan.garcia16@utp.edu.co>
2025-11-25 13:38:12 -0500

    migraciones y admin de django para los modelos listos

--- goals/admin.py
+++ goals/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import MetaAhorro
 # Register your models here.
+
+admin.site.register(MetaAhorro)
--- transactions/admin.py
+++ transactions/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Ingreso, Gasto
 # Register your models here.
+
+admin.site.register(Ingreso)
+admin.site.register(Gasto)
