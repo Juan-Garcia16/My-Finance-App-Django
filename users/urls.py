7fd29609019264abbfde24163b41badd5b2e0729
Juan Garcia <juan.garcia16@utp.edu.co>
2025-11-28 15:16:16 -0500

    actuazacion en urls

--- users/urls.py
+++ users/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
 	path('login/', views.CustomLoginView.as_view(), name='login'),
 	path('logout/', views.CustomLogoutView.as_view(), name='logout'),
 	path('register/', views.register_view, name='register'),
-	path('dashboard/', views.dashboard_view, name='dashboard'),
+	path('dashboard', views.dashboard_view, name='dashboard'),
 	path('profile/', views.profile_view, name='profile'),
 ]
